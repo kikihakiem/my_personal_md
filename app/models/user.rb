243926dcbe1f529b0280ceda9f0db33383c892f0
Kiki Luqman Hakiem <kiki.hakiem@gmail.com>
2020-07-08 12:16:44 +0700

    Login with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.image = auth.info.image
+      user.provider = auth.provider
+      user.uid = auth.uid
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.google_oauth2'] && session['devise.google_oauth2_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,6 +273,12 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  google_client_id = Rails.application.credentials.google_oauth2[:client_id]
+  google_client_secret = Rails.application.credentials.google_oauth2[:client_secret]
+  config.omniauth :google_oauth2, google_client_id, google_client_secret,
+                  scope: 'userinfo.email,userinfo.profile', skip_jwt: true,
+                  redirect_uri: 'http://localhost.com:3000/users/auth/google_oauth2/callback'
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
